d49d2894359b3b1b8d9a1b6f10bc6fe3dc058332
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2023-12-01 11:05:11 +0900

    아이템 리스트만 추가

--- Assets/UJY/Scripts/ItemData.cs
+++ Assets/UJY/Scripts/ItemData.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using static Info;
 
-public class ItemData : MonoBehaviour
+public class ItemData : Info
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip ItemSound;
+    public ItemType itype;
+    public enum ItemType
     {
-        
+        Apple,
+        Banana,
+        Kiwi,
+        Pineapple,
+        Mellon
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        SetItemStats();
+    }
+
+    void SetItemStats()
+    {
+        switch (itype)
+        {
+            case ItemType.Apple:
+                Hp = 10;
+                Atk = 0;
+                MoveSpeed = 0;
+                BulletRpm = 0;
+                break;
+            case ItemType.Banana:
+                Hp = 0;
+                Atk = 0;
+                MoveSpeed = 1;
+                BulletRpm = 0;
+                break;
+            case ItemType.Kiwi:
+                Hp = 0;
+                Atk = 0;
+                MoveSpeed = 0;
+                BulletRpm = 10;
+                break;
+            case ItemType.Pineapple:
+                Hp = -10;
+                Atk = -1;
+                MoveSpeed = -1;
+                BulletRpm = -10;
+                break;
+            case ItemType.Mellon:
+                Hp = 0;
+                Atk = 10;
+                MoveSpeed = 0;
+                BulletRpm = 0;
+                break;
+
+        }
     }
 }
