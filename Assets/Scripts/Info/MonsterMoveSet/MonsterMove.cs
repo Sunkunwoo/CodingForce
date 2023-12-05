ed6e25712821fd73b3c968a5b661f4260f7cd590
seungjun <149456880+el1win2@users.noreply.github.com>
2023-12-05 13:04:48 +0900

    몬스터 이동 구현

---
+++ Assets/Scripts/Info/MonsterMoveSet/MonsterMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMove : MonoBehaviour
+{
+    float limitY;
+
+    
+    void Start()
+    {
+        limitY = Random.Range(-0.5f, -1.5f);
+    }
+
+    void Update()
+    {
+        if (transform.position.y >= limitY) transform.position += new Vector3(0f, -0.05f, 0f);
+    }
+}
