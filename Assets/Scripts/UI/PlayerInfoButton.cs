41e1b7bb3cacc39d8fc805f6055d1fede78934ab
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2023-12-06 18:38:49 +0900

    아바타 변경 추가
    인게임에서 캐릭터 변경 추가
    캐릭터가 작게 나오는 버그 있어서 수정중

--- Assets/Scripts/UI/PlayerInfoButton.cs
+++ Assets/Scripts/UI/PlayerInfoButton.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class PlayerInfoButton : MonoBehaviour
 {
     public GameObject InfoUI;
+    public GameObject AvatarBtn;
+    public GameObject OptionBtn;
     // Start is called before the first frame update
     public void Onclick()
     {
         if (InfoUI.activeSelf == true)
+        {
             InfoUI.SetActive(false);
+            AvatarBtn.SetActive(false);
+            OptionBtn.SetActive(false);
+        }
+
         else if (InfoUI.activeSelf == false)
+        {
             InfoUI.SetActive(true);
+            AvatarBtn.SetActive(true);
+            OptionBtn.SetActive(true);
+        }
+
     }
 }
